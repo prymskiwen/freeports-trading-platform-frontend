a725640650a2f69c0cc4c29599d2b23362485b93
Nenad <jupiter9381@gmail.com>
2021-06-01 15:46:11 +1000

    Show declare new account modal

--- src/components/NostroAccounts/index.tsx
+++ src/components/NostroAccounts/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Button, Container, Grid, Fab, makeStyles } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Fab,
+  Grid,
+  makeStyles,
+  MenuItem,
+  TextField,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import purple from "@material-ui/core/colors/purple";
 import MaterialTable from "material-table";
@@ -39,6 +51,7 @@ const columns = [
     sortable: false,
     render: (rowData: any) => {
       const { nbr_trades: nbrTrades } = rowData;
+
       return (
         <div className="flex-center">
           {nbrTrades.map((ele: number) => {
@@ -60,18 +73,32 @@ const columns = [
 const useStyles = makeStyles({
   tableButton: {
     color: purple[500],
+    fontWeight: "bold",
   },
 });
 const NostroAccounts = (): React.ReactElement => {
   const classes = useStyles();
+  const [declareAccountModalOpen, setDeclareAccountModalOpen] =
+    React.useState(false);
+
+  const handleDeclareAccountModalOpen = () => {
+    setDeclareAccountModalOpen(true);
+  };
+
+  const handleDeclareAccountModalClose = () => {
+    setDeclareAccountModalOpen(false);
+  };
 
   return (
     <div className="main-wrapper">
       <Container>
-        <Grid container>
+        <Grid container spacing={3}>
           <Grid container item justify="flex-end" xs={12}>
             <Grid item>
-              <Button className={classes.tableButton}>
+              <Button
+                className={classes.tableButton}
+                onClick={handleDeclareAccountModalOpen}
+              >
                 <Fab
                   className="mr-10"
                   color="primary"
@@ -82,6 +109,73 @@ const NostroAccounts = (): React.ReactElement => {
                 </Fab>
                 Declare new account
               </Button>
+              <Dialog
+                open={declareAccountModalOpen}
+                onClose={handleDeclareAccountModalClose}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogTitle id="form-dialog-title">
+                  Declare New account
+                </DialogTitle>
+                <DialogContent>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <TextField
+                        margin="dense"
+                        id="account_name"
+                        label="Account name"
+                        type="text"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        margin="dense"
+                        id="account_number"
+                        label="Account number"
+                        type="number"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <TextField
+                        margin="dense"
+                        id="currency"
+                        label="Currency"
+                        variant="outlined"
+                        fullWidth
+                        select
+                      >
+                        <MenuItem value="10">Ten</MenuItem>
+                        <MenuItem value="20">Twenty</MenuItem>
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        margin="dense"
+                        id="initial_balance"
+                        label="Initial balance"
+                        type="number"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    onClick={handleDeclareAccountModalClose}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Create account
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </Grid>
             <Grid item>
               <Button className={classes.tableButton}>
@@ -97,16 +191,18 @@ const NostroAccounts = (): React.ReactElement => {
               </Button>
             </Grid>
           </Grid>
-          <Grid item xs={12} spacing={3}>
-            <MaterialTable
-              columns={columns}
-              data={data}
-              title="ACCOUNT ACTIVITIES"
-              options={{
-                search: true,
-                pageSize: 10,
-              }}
-            />
+          <Grid container>
+            <Grid item xs={12}>
+              <MaterialTable
+                columns={columns}
+                data={data}
+                title="ACCOUNT ACTIVITIES"
+                options={{
+                  search: true,
+                  pageSize: 10,
+                }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Container>
