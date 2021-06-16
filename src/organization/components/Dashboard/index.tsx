8b8999b596de40357285a7954959ec4b0ac28581
Muhammed <aapozaid@gmail.com>
2021-06-16 20:14:19 +0200

    Initialize organization routes

---
+++ src/organization/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const Dashboard = (): React.ReactElement => {
+  return (
+    <div className="main-wrapper">
+      <Container>
+        <span>Dashboard</span>
+      </Container>
+    </div>
+  );
+};
+
+export default Dashboard;
