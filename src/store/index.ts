194f1049764579859fac813817c842a0ce8d7995
Muhammed <aapozaid@gmail.com>
2021-06-18 03:08:26 +0200

    Create store directly to infer the types

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,27 @@
-import storeConfig from "./config";
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
 
-const store = storeConfig();
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./reducers";
+import rootSaga from "../sagas";
+
+const initialState = {};
+
+const sagaMiddleware = createSagaMiddleware();
+
+// Middleware and store enhancers
+const enhancers = [applyMiddleware(sagaMiddleware)];
+
+if (process.env.NODE_ENV !== "production") {
+  enhancers.push(applyMiddleware(createLogger()));
+  // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  //   enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__())
+}
+
+const store = createStore(rootReducer, initialState, compose(...enhancers));
+
+sagaMiddleware.run(rootSaga);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
