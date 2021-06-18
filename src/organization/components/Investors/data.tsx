25dc307784b21c38d1ae8e04c511084d919a9cd6
Ivanir Sabino <ivanirsabino88@gmail.com>
2021-06-18 19:00:41 -0300

    ADD INVESTORS PAGE TO ORGANIZATION INTERFACE
    Add investors page to organization interface
    New feature / Change #3488 is completed

---
+++ src/organization/components/Investors/data.tsx
@@ -0,0 +1,76 @@
+const data = [
+  {
+    id: 1,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 2,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 3,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 4,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 5,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 6,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 7,
+    investor_id: "Investor ID",
+    account_summary: "U$ 55'000",
+    trading_account_summary: "U$ 55'000",
+  },
+  {
+    id: 8,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 9,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 10,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 11,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+  {
+    id: 12,
+    investor_id: "Investor ID",
+    account_summary: "U$ 100'000",
+    trading_account_summary: "U$ 100'000",
+  },
+];
+
+export default data;
--- src/organization/components/Investors/index.tsx
+++ src/organization/components/Investors/index.tsx
@@ -1,11 +1,127 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Container,
+  createStyles,
+  Fab,
+  FormControl,
+  Grid,
+  makeStyles,
+  MenuItem,
+  Select,
+  Theme,
+  Typography,
+  Chip,
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import MaterialTable, {MTableHeader, MTableToolbar} from 'material-table';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import data from "./data";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    addButton: {
+      color: theme.palette.primary.main,
+      fontWeight: "bold",
+      marginRight: theme.spacing(2),
+      marginLeft: theme.spacing(2),
+    },
+    currencyDropdown: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    errorMessage: {
+      marginTop: theme.spacing(8),
+    },
+  })
+);
+
+const columns = [
+  {
+    field: "investor_iD",
+    title: "Investor ID",
+    cellStyle: {
+      width: "30%",
+    },
+    render: (rowData: any) => {
+      const { investor_id: investorID } = rowData;
+
+      return <Link to="/investors/detail">{investorID}</Link>;
+    },
+  },
+  {
+    field: "account_summary",
+    title: "Account summary",
+    cellStyle: {
+      width: "30%",
+    },
+  },
+  {
+    field: "trading_account_summary",
+    title: "Trading account summary",
+    cellStyle: {
+      width: "40%",
+    },
+  },
+];
+
+const currencyOptions = [{ name: "U$", value: "usd" }];
 
 const Investors = (): React.ReactElement => {
+  const classes = useStyles();
+
   return (
     <div className="main-wrapper">
       <Container>
-        <span>Investors</span>
+        <Grid container spacing={2}>
+          <Grid container item alignItems="center" xs={12}>
+            <Typography variant="h4">Investors</Typography>
+            <Button className={classes.addButton}>
+              <Fab
+                className="mr-10"
+                color="primary"
+                aria-label="Add"
+                size="small"
+              >
+                <AddIcon fontSize="small" />
+              </Fab>
+            </Button>
+          </Grid>
+          <Grid container item alignItems="center" justify="flex-end" xs={12}>
+            <Typography variant="subtitle1">
+              Preferred currency display:{" "}
+            </Typography>
+            <FormControl
+              size="small"
+              variant="outlined"
+              className={classes.currencyDropdown}
+            >
+              <Select value="usd">
+                {currencyOptions.map((opt) => (
+                  <MenuItem value={opt.value}>{opt.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid container>
+            <Grid item xs={12}>
+              <MaterialTable
+                title=""
+                columns={columns}
+                data={data}
+                options={{
+                  search: true,
+                  pageSize: 10,
+                  filtering: false,
+                  searchFieldAlignment: "left",
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
