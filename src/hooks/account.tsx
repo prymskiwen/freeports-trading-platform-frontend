cb32661c10c80d181b9b07884d1e007879c074ff
Muhammed <aapozaid@gmail.com>
2021-06-18 18:10:21 +0200

    version increment

--- src/hooks/account.tsx
+++ src/hooks/account.tsx
@@ -1,23 +1,29 @@
-import { getAllAccounts, assignOrganizationAccount } from "../services/accountService";
+import {
+  getAllAccounts,
+  assignOrganizationAccount,
+} from "../services/accountService";
 
 function useAccounts(): any {
   const allAccounts = async () => {
-    const accounts = await getAllAccounts()
-      .then((data: any) => {
-        return data;
-      })
+    const accounts = await getAllAccounts().then((data: any) => {
+      return data;
+    });
     return accounts;
-  }
-  const assignAccount = async (organizerId: string, accountId: string): Promise<any> => {
+  };
+  const assignAccount = async (
+    organizerId: string,
+    accountId: string
+  ): Promise<any> => {
     const assign = await assignOrganizationAccount(organizerId, accountId)
       .then((data: any) => {
         return data;
-      }).catch((err) => {
-        console.log(err);
       })
+      .catch((err) => {
+        console.log(err);
+      });
     return assign;
-  }
-  return { allAccounts, assignAccount }
+  };
+  return { allAccounts, assignAccount };
 }
 
 export default useAccounts;
