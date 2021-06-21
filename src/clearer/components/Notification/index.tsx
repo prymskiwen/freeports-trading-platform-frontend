7cfbfd88dcee29521f6168111698d4f0c68b9964
goodlancer <forethink888@gmail.com>
2021-06-21 23:18:18 +0300

    notification UI update
    notification UI update

--- src/clearer/components/Notification/index.tsx
+++ src/clearer/components/Notification/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Grid, Icon, Button, IconButton, FormControl, InputLabel, Select } from "@material-ui/core";
+import { Grid, Icon, Button, IconButton, NativeSelect, FormControl, Typography, InputLabel, Select } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { DropdownButton, Dropdown, Card } from "react-bootstrap";
 
 import useAuth from "../../../hooks";
 
@@ -15,6 +15,12 @@ const useStyle = makeStyles((theme) => ({
     top: 0,
     boxShadow: '0 0 17px 0',
     display: 'flex',
+  },
+  margin0:{
+    margin: 0,
+  },
+  padding5: {
+    padding: 15,
   },
   nofifyBtn: {
     width: 50,
@@ -53,6 +59,11 @@ const useStyle = makeStyles((theme) => ({
   },
   alertBar: {
     width: 450,
+    padding: 20,
+  },
+  notiTypeSpan: {
+    fontWeight: 600,
+    fontSize: 20,
   }
 }))
 
@@ -93,16 +104,43 @@ const Notification =(): React.ReactElement => {
                   <Button>&gt;Open as Page</Button>
                   <Button>&gt;History</Button>
                 </Grid>
-                <Grid item xs={12}>
-                  <span>Notification</span>
-                  <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                  </DropdownButton>
+                <Grid container direction="row" justify="space-around" alignItems="center" item xs={12}>
+                  <span className={classes.notiTypeSpan}>Notification Type</span>
+                  <FormControl className={classes.formControl}>
+                    <NativeSelect
+                      defaultValue={30}
+                    >
+                      <option value={10}>Re-funding</option>
+                      <option value={20}>Re-funding</option>
+                      <option value={30}>Re-funding</option>
+                    </NativeSelect>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={12}>
-                  sdfsdf
+                  <Card className={classes.padding5}>
+                    <div>
+                      <h3 className={classes.margin0}>Re-funding / 23.06.2021 - 14:12</h3>
+                      <h2 className={classes.margin0}>Notification title</h2>
+                      <h3 className={classes.margin0}>From (name)</h3>
+                      <div>
+                        <Typography>
+                          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </Typography>
+                      </div>
+                    </div>
+                  </Card>
+                  <Card className={classes.padding5}>
+                    <div>
+                      <h3 className={classes.margin0}>Re-funding / 23.06.2021 - 14:12</h3>
+                      <h2 className={classes.margin0}>Notification title</h2>
+                      <h3 className={classes.margin0}>From (name)</h3>
+                      <div>
+                        <Typography>
+                          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </Typography>
+                      </div>
+                    </div>
+                  </Card>
                 </Grid>
               </Grid>
             </div>
