cb062910fbf11e9ce752510d6b5eca118b8aaf1b
goodlancer <forethink888@gmail.com>
2021-06-22 18:13:18 +0300

    update work
    update work

--- src/clearer/components/Organisations/AddOrganizer/index.tsx
+++ src/clearer/components/Organisations/AddOrganizer/index.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {  Container,
           Grid,
-          OutlinedInput,
-          InputAdornment,
-          InputLabel,
           FormControl,
           Select,
           MenuItem,
