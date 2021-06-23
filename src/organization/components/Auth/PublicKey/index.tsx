91afbd80f8c18170a700d81159db9ef439a65158
goodlancer <forethink888@gmail.com>
2021-06-23 06:08:17 +0300

    UI for the public key missing banner
    UI for the public key missing banner

--- src/organization/components/Auth/PublicKey/index.tsx
+++ src/organization/components/Auth/PublicKey/index.tsx
@@ -1,9 +1,35 @@
-import react from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { Snackbar, Slide } from "@material-ui/core";
+import { TransitionProps } from "@material-ui/core/transitions";
+
+import useAuth from "../../../../hooks";
+
+// function TransitionDown(props: TransitionProps) {
+//   return <Slide {...props} direction="down" />;
+// }
 
 const PublicKeyBanner = (): React.ReactElement =>{
+  const history = useHistory();
+  const [opened, setOpened] = useState(true);
+  const { isAuthenticated, signOut } = useAuth();
+  const vertical = 'top';
+  const horizontal = 'center';
+  const messageInfo = 'snaxBar';
+  const handleClose = () => {
+    setOpened(false);
+    history.push(`/profile`);
+  }
   return (
     <div>
-      sdsd
+      <Snackbar
+        key={messageInfo}
+        open={opened}
+        autoHideDuration={3000}
+        anchorOrigin={{vertical, horizontal}}
+        onClose={handleClose}
+        message="missing public key"
+      />
     </div>
   )
 }
