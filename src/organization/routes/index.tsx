4abb3b00606a9b7367ec15125af16ff8e9a4c371
Nenad <jupiter9381@gmail.com>
2021-06-30 19:46:48 +0200

    add more margin to main wrapper

--- src/organization/routes/index.tsx
+++ src/organization/routes/index.tsx
@@ -3,6 +3,7 @@
 // import libs
 import React, { useState } from "react";
 import { BrowserRouter as Router, Redirect, Switch } from "react-router-dom";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 
 // import components
 import routes from "./routes";
@@ -11,7 +12,15 @@ import PublicRoute from "../../routes/public";
 import Header from "../components/Header";
 import NotificationCenter from "../../components/NotificationCenter";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      padding: theme.spacing(4),
+    },
+  })
+);
 const Routes = (): React.ReactElement => {
+  const classes = useStyles();
   const [notificationDrawerOpen, setNotificationDrawerOpen] = useState(false);
 
   const handleNotificationDrawerOpen = () => {
@@ -32,17 +41,18 @@ const Routes = (): React.ReactElement => {
       <Router>
         <>
           <Header {...headerProps} />
-          <Switch>
-            {routes.map((route, i) => {
-              if (route.auth) {
-                return <PrivateRoute key={i} {...route} />;
-              }
-              // if (error.errorType !== "") return <Redirect to="/error" />;
-              return <PublicRoute key={i} {...route} />;
-            })}
-            <Redirect to="/dashboard" />
-          </Switch>
-
+          <main className={classes.main}>
+            <Switch>
+              {routes.map((route, i) => {
+                if (route.auth) {
+                  return <PrivateRoute key={i} {...route} />;
+                }
+                // if (error.errorType !== "") return <Redirect to="/error" />;
+                return <PublicRoute key={i} {...route} />;
+              })}
+              <Redirect to="/dashboard" />
+            </Switch>
+          </main>
           <NotificationCenter {...drawerProps} />
         </>
       </Router>
