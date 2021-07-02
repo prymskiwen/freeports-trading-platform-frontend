d6ea420a377bc26403ded724230c0ec4f32e72f1
Nenad <jupiter9381@gmail.com>
2021-07-02 07:29:40 +0200

    refs #3545: organization settings page

--- src/clearer/components/Header/index.tsx
+++ src/clearer/components/Header/index.tsx
@@ -23,6 +23,7 @@ import {
   Home,
   Notifications,
   Person,
+  Settings,
 } from "@material-ui/icons";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
@@ -185,8 +186,13 @@ const Header = ({
                     <ListItemIcon>
                       <GroupIcon fontSize="small" />
                     </ListItemIcon>
-
                     <ListItemText primary="Co-workers" />
+                  </MenuItem>
+                  <MenuItem onClick={() => redirect("/settings")}>
+                    <ListItemIcon>
+                      <Settings fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
                   </MenuItem>
                   <Divider />
                   <MenuItem onClick={signOut}>
